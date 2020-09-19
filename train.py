59d0ddc691168b33137c3add9c4246ee23a61b7c
Cassidy McDonnell <cmcd724@gmail.com>
2020-09-19 10:42:12 -0700

    Define transforms

--- train.py
+++ train.py
@@ -12,3 +12,22 @@ from collections import OrderedDict
 import helper
 from PIL import Image
 import numpy as np
+
+#define transforms for training, validation, and testing sets
+
+train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.ToTensor(),  
+                                       transforms.Normalize([0.485, 0.456, 0.406],
+                                                            [0.229, 0.224, 0.225])])
+
+valid_transforms = transforms.Compose([transforms.RandomResizedCrop(224),
+                                       transforms.ToTensor(),  
+                                       transforms.Normalize([0.485, 0.456, 0.406],
+                                                            [0.229, 0.224, 0.225])])
+
+testing_transforms = transforms.Compose([transforms.RandomResizedCrop(224),
+                                       transforms.ToTensor(),  
+                                       transforms.Normalize([0.485, 0.456, 0.406],
+                                                            [0.229, 0.224, 0.225])])
