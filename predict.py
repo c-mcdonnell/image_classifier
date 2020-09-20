2678e3eb2d690473d922717e6abeba8637784e7e
Cassidy McDonnell <cmcd724@gmail.com>
2020-09-20 15:39:14 -0700

    initial commit

---
+++ predict.py
@@ -0,0 +1,22 @@
+#load the checkpoint
+def load_checkpoint(filepath):
+    checkpoint = torch.load(filepath)
+    model = models.vgg16(pretrained = True)
+
+    #load saved attributes to model
+    model.batch_size = checkpoint['batch_size']
+    model.learning_rate = checkpoint['learing_rate']
+    model.state_dict = checkpoint['state_dict']
+    model.optimizer = checkpoint['optimizer']
+    model.input_size = checkpoint['input_size']
+    model.output_size = checkpoint['output_size']
+    model.criterion = checkpoint['criterion']
+    model.class_to_idx = checkpoint['class_to_idx']
+
+    #freeze parameters to avoid backpropogation
+    for param in model.parameters():
+        param.requires_grad = False
+
+    return model
+
+saved_model = load_checkpoint('checkpoint.pth');
