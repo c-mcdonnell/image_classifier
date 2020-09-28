e2588732453b9111edd4142c9a674a3b2f5327aa
Cassidy <cmcd724@gmail.com>
2020-09-28 11:35:41 -0700

    update parser - include input & save checkpoint

--- train.py
+++ train.py
@@ -18,7 +18,8 @@ import argparse
 #check command line arguments
 #check_command_line_arguments(in_args)
 parser = argparse.ArgumentParser()
-parser.add_argument('--save_dir', dest = 'checkpoint.pth', action = 'store', type = str, default='yes', help = 'Would you like to save the checkpoint after training?')
+parser.add_argument('data_dir', default = 'flowers/train')
+parser.add_argument('--save_dir', dest = 'checkpoint_dir', action = 'store', type = str, help = 'Would you like to save the checkpoint after training?')
 parser.add_argument('--arch', type=str, default='vgg16', help = 'CNN model archiecture, default = vgg16')
 parser.add_argument('--learning_rate', type = int, default=0.0001, help = 'set learning rate for model')
 parser.add_argument('--hidden_units', type = int, default = 4096, help = 'set number of hidden units in model')
